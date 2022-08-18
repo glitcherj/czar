e6d801605d43e3129a15602ab249be12fa628f9c
Jay <it6.mr.j@gmail.com>
2022-08-18 12:57:01 +0430

    Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,110 @@
+import "../../Styles/components/_footer.scss";
+import logoBW from "../../assets/logoBW.svg";
+import logoBG from "../../assets/LogoBG.svg";
+import facebook from "../../assets/socials/facebook.svg";
+import instagram from "../../assets/socials/instagram.svg";
+import twitter from "../../assets/socials/twitter.svg";
+import youtube from "../../assets/socials/youtube.svg";
+
+export function Footer() {
+  return (
+    <footer>
+      <section>
+        <div className="aboutCompany">
+          <a href="/">
+            <img className="appLogo" src={logoBW} alt="logo" />
+          </a>
+          <span>
+            CZAR PCO Ltd. is a leading London taxi hire firm established in
+            2006, and since then they have helped various taxi drivers get on
+            the road.
+          </span>
+        </div>
+        <div className="footerColumns">
+          <span className="footerColumnTitle">
+            <strong>Sitemap</strong>
+          </span>
+          <div className="footerColumnsContent">
+            <a href="/">Home page</a>
+            <a href="/">Our fleet</a>
+            <a href="/">EV fleet</a>
+            <a href="/">Blog & news</a>
+            <a href="/">About us</a>
+          </div>
+        </div>
+        <div className="footerColumns">
+          <span className="footerColumnTitle">
+            <strong>Support</strong>
+          </span>
+          <div className="footerColumnsContent">
+            <a href="/">FAQ</a>
+            <a href="/">Privacy policy</a>
+            <a href="/">Terms & Conditions</a>
+          </div>
+        </div>
+        <div className="footerColumns">
+          <span className="footerColumnTitle">
+            <strong>Company</strong>
+          </span>
+          <div className="footerColumnsContent">
+            <div className="footerColumnsContentRow">
+              <span className="footerColumnsContentTitle">Address:</span>
+              <span className="footerColumnsContentDescription">
+                98 Abingdon Road, BRANSTONE, London, PO36 6DN
+              </span>
+            </div>
+            <div className="footerColumnsContentRow">
+              <span className="footerColumnsContentTitle">Phone:</span>
+              <a
+                className="footerColumnsContentDescription"
+                href="tel:02089009574"
+              >
+                077-0172-7142
+              </a>
+            </div>
+            <div className="footerColumnsContentRow">
+              <span className="footerColumnsContentTitle">Email:</span>
+              <span
+                className="footerColumnsContentDescription"
+                href="mailto:info@czarpco.co.uk"
+              >
+                info@czarpco.co.uk
+              </span>
+            </div>
+            <div className="footerColumnsContentRow">
+              <span className="footerColumnsContentTitle">Opening hours:</span>
+              <span className="footerColumnsContentDescription">
+                Monday to Friday From 9:00 to 17:00
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="footerBottom">
+        <div className="footerBottomContent">
+          <span>© 2022 CZAR PCO Ltd. All rights reserved.</span>
+          <div className="socialLinks">
+            <a href="twitter.com/czarpco/">
+              <img src={twitter} alt="twitter" />
+            </a>
+            <a href="facebook.com/czarpco/">
+              <img src={facebook} alt="facebook" />
+            </a>
+            <a href="instagram.com/czarpco/">
+              <img src={instagram} alt="instagram" />
+            </a>
+            <a href="youtube.com/czarpco/">
+              <img src={youtube} alt="youtube" />
+            </a>
+          </div>
+        </div>
+        <span className="copyright">
+          Copyright @ 2020 | CZAR PCO is Authorized and Regulated by Financial
+          Conduct Authority (FCA). FCA Reference Number: 245190
+        </span>
+      </section>
+      <img src={logoBG} alt="logo" className="logoBG TL" />
+      <img src={logoBG} alt="logo" className="logoBG BR" />
+    </footer>
+  );
+}
