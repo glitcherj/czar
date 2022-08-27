3dd3e49a6be45c958786de257722fda52cb9acc2
Jay <it6.mr.j@gmail.com>
2022-08-27 20:38:09 +0430

    revert in routes

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -23,16 +23,6 @@ export function MainRouter() {
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms" element={<Terms />} />
       </Routes>
-      <Routes>
-        <Route path="/czar/" element={<Home />} />
-        <Route path="/czar/about-us" element={<AboutUs />} />
-        <Route path="/czar/blog" element={<Blog />} />
-        <Route path="/czar/ev-fleet" element={<EVFleet />} />
-        <Route path="/czar/fleet" element={<Fleet />} />
-        <Route path="/czar/FAQ" element={<FAQ />} />
-        <Route path="/czar/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/czar/terms" element={<Terms />} />
-      </Routes>
     </Router>
   );
 }
